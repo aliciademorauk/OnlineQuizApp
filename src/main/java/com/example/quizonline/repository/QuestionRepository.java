c7e3157a307e42652fb97c062d023fb2ccb7264d
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-03-29 15:29:43 +0000

    Add implementation to custom method repository interface

--- src/main/java/com/example/quizonline/repository/QuestionRepository.java
+++ src/main/java/com/example/quizonline/repository/QuestionRepository.java
@@ -4,11 +4,13 @@ import com.example.quizonline.model.Question;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<String> findDistinctSubjects();
 
+    @Query("SELECT DISTINCT question.subject FROM Question question")
+    List<String> findDistinctSubjects();
     Page<Question> findBySubject(String subject, Pageable pageable);
 }
