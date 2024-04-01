7377c45022b0b948f926a93a2e2a00b9d8f5865e
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-01 22:11:34 +0100

    Add functional component for rendering answer options Dynamic rendering based on questionType: for 'single', radio buttons; for 'multiple' select, checkboxes"

---
+++ Client/online-quiz-app/utils/AnswerOptions.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const AnswerOptions = ({question, isChecked, handleChangeAnswer, handleChangeCheckbox}) => {
+    if (!question) {
+        return <div>No questions available.</div>
+    }
+
+    const {id, questionType, choices} = question
+
+    if (questionType === 'single') {
+    return (
+    <div>
+        {choices.sort().map((choice, index) => (
+            <div key={choice} className='form-check mb-3'>
+                <input
+                    className='form-check-input'
+                    type='radio'
+                    id={choice}
+                    value={choice}
+                    checked={isChecked(question.id, choice)}
+                    onChange={() => handleChangeAnswer(id, choice)}/>
+                <label className='form-check-label ms-2'>{choice}</label>
+            </div>
+        ))}
+    </div>
+  )} else if (questionType === 'multiple') {
+    return (
+    <div>
+        <p>Select all that apply:</p>
+        {choices.sort().map((choice, index) => (
+            <div key={choice} className='form-check mb-3'>
+                <input
+                    className='form-check-input'
+                    type='checkbox'
+                    id={choice}
+                    value={choice}
+                    name={question.id}
+                    checked={isChecked(question.id, choice)}
+                    onChange={() => handleChangeCheckbox(id, choice)}/>
+                <label className='form-check-label ms-2'>{choice}</label>
+            </div>
+        ))}
+    </div>
+    )
+  } else {
+    return null
+  }
+}
+
+export default AnswerOptions
