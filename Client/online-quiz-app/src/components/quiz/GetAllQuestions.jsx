069ce9da43769603d859203d80b33b9daf0c99f0
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-01 18:16:29 +0100

    Clean up and modify function names

--- Client/online-quiz-app/src/components/quiz/GetAllQuestions.jsx
+++ Client/online-quiz-app/src/components/quiz/GetAllQuestions.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { deleteQuestion, getAllQuestions } from '../../utils/QuizService'
 
 const GetAllQuestions = () => {
-    const [question, setQuestions] = useState([
-        { id: '', question: '', correctAnswers: '', choices: [] }])
+    const [questions, setQuestions] = useState([
+        { id: '', question: '', correctAnswers: '', choices: [] }
+    ])
     const [isLoading, setIsLoading] = useState(true)
     const [isQuestionDeleted, setIsQuestionDeleted] = useState(false)
     const [deleteSuccessMessage, setDeleteSuccessMessage] = useState('')
@@ -11,6 +12,7 @@ const GetAllQuestions = () => {
     useEffect(() => {
         fetchAllQuestions()
     }, [])
+
     const fetchAllQuestions = async () => {
         try {
             const data = await getAllQuestions()
@@ -21,7 +23,7 @@ const GetAllQuestions = () => {
         }
     }
 
-    const handleDelete = async(id) => {
+    const handleDeleteQuestion = async(id) => {
         try {
             await deleteQuestion(id)
             setQuestions(questions.filter((question) => question.id !== id))
@@ -31,14 +33,48 @@ const GetAllQuestions = () => {
             console.error(error)
         }
         setTimeout(() => {
-            setDeleteSuccessMessage()
+            setDeleteSuccessMessage('')
         }, 4000)
     }
     
     if(isLoading) {
         return <div>Loading...</div>
     }
-    return ()
+    return (
+		<section className='container'>
+			<div className='row mt-5'>
+				<div className='col-md-6 mb-2 md-mb-0' style={{ color: 'GrayText' }}>
+					<h4>All Questions</h4>
+				</div>
+				<div className='col-md-4 d-flex justify-content-end'>
+                    {/* To do: add link to update question */}
+				</div>
+			</div>
+			<hr />
+			{isQuestionDeleted && <div className='alert alert-success'>{deleteSuccessMessage}</div>}
+			{questions.map((question, index) => (
+				<div>
+						<h4 style={{ color: 'GrayText' }}>{`${index + 1}. ${question.question}`}</h4>
+					<ul>
+						{question.choices.map((choice, index) => (
+							<li key={index}>{choice}</li>
+						))}
+					</ul>
+					<p className='text-success'>Correct Answer: {question.correctAnswers}</p>
+					<div className='btn-group mb-4'>
+						<Link to={`/update-quiz/${question.id}`}>
+							<button className='btn btn-sm btn-outline-warning mr-2'>Edit Question</button>
+						</Link>
+						<button
+							className='btn btn-sm btn-outline-danger'
+							onClick={() => handleDeleteQuestion(question.id)}>
+							Delete Question
+						</button>
+					</div>
+				</div>
+			))}
+		</section>
+	)
 }
 
 export default GetAllQuestions
