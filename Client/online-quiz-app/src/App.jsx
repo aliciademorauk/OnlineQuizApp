3f552bb2701bb6b102029938b5c945032bb97f54
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-02 02:07:41 +0100

    Fix imports that included deleted files and code Admin.jsx, Home.jsx

--- Client/online-quiz-app/src/App.jsx
+++ Client/online-quiz-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AddQuestion from './components/question/AddQuestion'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
 
 function App() {
   return (
