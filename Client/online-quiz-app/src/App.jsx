2dee20c18bd3447b59b6f438c5a1080071428717
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-02 01:40:11 +0100

    Declare application routes in App.jsx with BrowserReact

--- Client/online-quiz-app/src/App.jsx
+++ Client/online-quiz-app/src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AddQuestion from './components/question/AddQuestion'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -6,15 +5,21 @@ import './App.css'
 
 function App() {
   return (
-    function App() {
-      return (
-        <Router>
-          <Routes>
-            <Route path="/" element={<AddQuestion />} />
-          </Routes>
-        </Router>
-      );
-    }
+    <main className='container mt-5 mb-5'>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Home />} />
+					<Route path="/quiz-stepper" element={<QuizStepper />} />
+					<Route path="/take-quiz" element={<Quiz />} />
+					<Route path="/admin" element={<Admin />} />
+
+					<Route path="/create-quiz" element={<AddQuestion />} />
+					<Route path="/update-quiz/:id" element={<UpdateQuestion />} />
+					<Route path="/all-quizzes" element={<GetAllQuiz />} />
+					<Route path="/quiz-result" element={<QuizResult />} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
