e4c0c2e565800a95cf155663f8ceb2272519ab92
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-12 18:07:43 +0100

    Remove Bootstrap import

--- Client/online-quiz-app/src/App.jsx
+++ Client/online-quiz-app/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import QuizStepper from './components/quiz/QuizStepper'
